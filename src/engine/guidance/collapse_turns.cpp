a81302a18b2da7b5de7e69d7e6e7c0379debe4b4
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2018-02-01 12:42:20 +0000

    Fix multi-line warning

--- src/engine/guidance/collapse_turns.cpp
+++ src/engine/guidance/collapse_turns.cpp
@@ -53,7 +53,7 @@ double findTotalTurnAngle(const RouteStep &entry_step, const RouteStep &exit_ste
     // both angles are in the same direction, the total turn gets increased
     //
     // a ---- b
-    //           \
+    //           `
     //              c
     //              |
     //              d
